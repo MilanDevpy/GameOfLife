0fd39b02c31973d10286fc57416ed7a24671980e
MilanDevPy <amaury.notin@yahoo.com>
2026-09-21 10:58:18 +0200

    Finished !

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,34 @@
 use ::rand::random_range;
+use image::{
+    Delay, Frame,
+    codecs::gif::{GifEncoder, Repeat},
+};
 use macroquad::{input::KeyCode::Space, prelude::*};
-mod generate;
-
-const SCREEN_SIZE: (u32, u32) = (500, 500); //(x,y)
+use std::fs::File;
+const SCREEN_SIZE: (u32, u32) = (700, 700); //(x,y)
 const BG_COLOR: Color = BLACK;
 const FULL_SCREEN: bool = false;
 const WAIT_FOR_SIGNAL: bool = true;
 
-const EXPORTING: bool = true;
-const EXPORTING_RATE: u16 = 1;
-const SCREEN_LIMIT: u16 = 300;
+const EXPORTING: bool = false;
+const EXPORTING_RATE: u16 = 5;
+const SCREEN_LIMIT: u16 = 200;
+const DELAY: u8 = 20;
 const EXPORT_DIR: &str = "/home/cookie/Documents/Dev/Rust/Bordel Evolutif/gof_v2/Frames/frame";
 
-const SPAWN_TYPE: SpawnType = SpawnType::FullCircle;
-const SPAWN_RATE: u8 = 2; // 1 chance sur SPAWN_RATE pour une cellule de spawn vivante
+const SPAWN_TYPE: SpawnType = SpawnType::FullRandom;
+const SPAWN_RATE: u8 = 6;
 const CIRCLE_ROUND: u32 = 70;
-const CIRCLE_WEIGHT: u32 = 50;
+const CIRCLE_WEIGHT: u32 = 20;
 enum SpawnType {
     FullRandom,
     FullCircle,
 }
 //  Classic : b: 2,3 s: 3
 const CELL_TYPE: CellType = CellType {
-    b: &[1],
-    s: &[1],
-    color: WHITE,
+    b: &[2, 3, 4],
+    s: &[],
+    color: YELLOW,
 };
 
 #[derive(PartialEq, Copy, Clone)]
@@ -133,31 +137,56 @@ async fn main() {
     let mut population: Vec<CellState> = init_pop();
     request_new_screen_size(SCREEN_SIZE.0 as f32, SCREEN_SIZE.1 as f32);
     set_fullscreen(FULL_SCREEN);
+    let file = File::create("output.gif").unwrap();
+    let mut encoder = GifEncoder::new_with_speed(file, 10);
+    encoder.set_repeat(Repeat::Infinite).unwrap();
+
     if WAIT_FOR_SIGNAL {
         while !is_key_down(Space) {
             next_frame().await
         }
     }
+    let mut captured = 0;
+
     loop {
-        clear_background(BG_COLOR);
+        let mut image_frame =
+            Image::gen_image_color(SCREEN_SIZE.0 as u16, SCREEN_SIZE.1 as u16, BG_COLOR);
         let buffer_pop: Vec<CellState> = population.clone();
         for y in 0..SCREEN_SIZE.1 {
             for x in 0..SCREEN_SIZE.0 {
                 let cell_state = scan_cell((x, y), &buffer_pop);
                 population[ptinx(x, y) as usize] = cell_state;
                 if cell_state == CellState::Alive {
-                    draw_rectangle(x as f32, y as f32, 1.0, 1.0, CELL_TYPE.color);
+                    image_frame.set_pixel(x, y, CELL_TYPE.color);
                 };
             }
         }
-        if instance % EXPORTING_RATE as u64 == 0
-            && instance / EXPORTING_RATE as u64 <= SCREEN_LIMIT as u64
-            && EXPORTING
-        {
-            println!("screenshit {instance}");
-            get_screen_data().export_png(format!("{EXPORT_DIR}_{instance}.png").as_str());
+        if instance % EXPORTING_RATE as u64 == 0 && captured < SCREEN_LIMIT && EXPORTING {
+            captured += 1;
+            image_frame.export_png(format!("{EXPORT_DIR}_{instance}.png").as_str());
+            //println!("screenshit {instance}");
+            let img = image::open(format!("{EXPORT_DIR}_{instance}.png"))
+                .unwrap()
+                .to_rgba8();
+
+            encoder
+                .encode_frame(Frame::from_parts(
+                    img,
+                    0,
+                    0,
+                    Delay::from_numer_denom_ms(DELAY as u32, 1),
+                ))
+                .unwrap();
+        } else if SCREEN_LIMIT <= captured {
+            break;
         }
         instance += 1;
+        draw_texture(
+            &macroquad::texture::Texture2D::from_image(&image_frame),
+            0.0,
+            0.0,
+            WHITE,
+        );
         next_frame().await;
     }
 }
